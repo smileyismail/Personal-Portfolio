d4c624da8bdb162b36094dcb8c6e2216a216d539
Smiley Ismail <smileyismail67@gmail.com>
2022-09-06 01:50:21 +0530

    netlify deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import Styles from "./index.css";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
