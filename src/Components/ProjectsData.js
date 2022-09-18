658738c2318fad033110ba12271110c55662fddb
smileyismail <79481480+smileyismail@users.noreply.github.com>
2022-09-18 22:42:21 +0530

    more projects added

--- src/Components/ProjectsData.js
+++ src/Components/ProjectsData.js
@@ -4,6 +4,8 @@ import { CgProfile } from "react-icons/cg";
 import { GiClockwork } from "react-icons/gi";
 import { MdOutlineLocalMovies } from "react-icons/md";
 import { RiNewspaperLine } from "react-icons/ri";
+import { BsCart4 } from "react-icons/bs";
+import { FaCalculator } from "react-icons/fa";
 
 const PROJECTS_DATA = [
   {
@@ -23,7 +25,7 @@ const PROJECTS_DATA = [
     category: "javaScript",
     description:
       "A retro game studio where one can enjoy the 90s games. Some games are also integrated with API's and Computer AI",
-    toolsUsed: "JavaScript, CSS, HTML",
+    toolsUsed: "HTML, CSS, JavaScript",
     icon: <CgGames />,
     githubLink: "https://github.com/smileyismail/Mini-Games-Studio",
     liveLink: "https://smiley-mini-games-studio.netlify.app/",
@@ -57,7 +59,7 @@ const PROJECTS_DATA = [
     category: "javaScript",
     description:
       "A website where you can find latest movies with details along with their ratings. The application is connected to a API so that i fetches latest Movies all the time.",
-    toolsUsed: "React, CSS, JSX",
+    toolsUsed: "HTML, CSS, JavaScript",
     icon: <MdOutlineLocalMovies />,
 
     githubLink: "https://github.com/smileyismail/Movies-Palace",
@@ -74,6 +76,30 @@ const PROJECTS_DATA = [
 
     githubLink: "https://github.com/smileyismail/Resume-Builder",
     liveLink: "https://smiley-resume-builder.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "E-Commerce Store",
+    category: "react",
+    description:
+      "This is an E-Commerce Store with cart option and the can be filtered by category. We can also search products by their names.",
+    toolsUsed: "React, CSS, JSX",
+    icon: <BsCart4 />,
+
+    githubLink: "https://github.com/smileyismail/E-Commerse-Store",
+    liveLink: "https://smiley-e-store.netlify.app/",
+  },
+  {
+    id: 8,
+    title: "Calculator",
+    category: "javaScript",
+    description:
+      "A calculator that can be used to perform all the mathematical operations.",
+    toolsUsed: "HTML, CSS, JavaScript",
+    icon: <FaCalculator />,
+
+    githubLink: "https://github.com/smileyismail/Calculator",
+    liveLink: "https://smiley-calculator.netlify.app/",
   },
 ];
 
