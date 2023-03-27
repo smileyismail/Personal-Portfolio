e64c60a1be9151e233a64c41117d1891c2f626cd
smileyismail <smileyismail67@gmail.com>
2023-03-27 23:47:49 +0530

    preloader added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
+import BounceLoader from "react-spinners/BounceLoader";
 
 import Header from "./Components/Header/Header";
 
@@ -14,31 +16,57 @@ import Footer from "./Components/Footer/Footer";
 import Works from "./Components/Works/Works";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div className="bg-primary text-customText">
-      <Header />
-      <main className="w-[80vw] mx-auto flex flex-col justify-center items-center">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <SocialMedia />
-                <SocialEmail />
-                <Hero />
-                <About />
-                <Skills />
-                <Works />
-                <Projects />
-                <Contact />
-                <Footer />
-              </>
-            }
+      {loading ? (
+        <div className="w-screen h-screen flex justify-center items-center">
+          <BounceLoader
+            color={"#00ADB5"}
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
           />
+        </div>
+      ) : (
+        <>
+          <Header />
+          <main className="w-[80vw] mx-auto flex flex-col justify-center items-center">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <SocialMedia />
+                    <SocialEmail />
+                    <Hero />
+                    <About />
+                    <Skills />
+                    <Works />
+                    <Projects />
+                    <Contact />
+                    <Footer />
+                  </>
+                }
+              />
 
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </main>
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </main>
+        </>
+      )}
     </div>
   );
 };
